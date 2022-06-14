60fbb2ad3ede6823e3b3fc7b70ca28502b999b9b
juanca-dev <juancarlos17.mf@gmail.com>
2022-06-14 15:34:06 -0500

    puerto actualizado 2

--- SRC/index.js
+++ SRC/index.js
@@ -15,4 +15,4 @@ app.use(indexRoutes)
 app.use(express.static(join(__dirname, 'public')))
 
 app.listen(process.env.PORT || 3000)
-console.log('Server is listening on port', process.env.PORT ||3000)
+console.log('Server is listening on port', process.env.PORT || 3000)
